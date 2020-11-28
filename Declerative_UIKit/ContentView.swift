7d3d5f1c03c5dff04df0342c4960bd743ff24e73
Geri Borbás <geri@eppz.eu>
2020-11-28 22:42:41 +0100

    Initial Commit

---
+++ Declerative_UIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Declerative_UIKit
+//
+//  Created by Geri Borbás on 28/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
